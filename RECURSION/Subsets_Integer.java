306240ee6d933ef2c13407cdbf0a24890c77c821
soumyabag <67749624+soumyabag@users.noreply.github.com>
2022-07-19 08:36:37 +0530

    Add files via upload

---
+++ RECURSION/Subsets_Integer.java
@@ -0,0 +1,30 @@
+package DSA;
+import java.util.*;
+
+public class Subsets_Integer {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        int[] nums = {1,2,3};
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> output = new ArrayList<>();
+        sub(nums,0,output,ans);
+        System.out.println(ans);
+	}
+	
+	public static void sub(int[] nums, int i, List<Integer> output, List<List<Integer>> ans){
+        if(i >= nums.length){
+            ans.add(new ArrayList<>(output));
+            return;
+        }
+       
+        // include
+        int element = nums[i];
+        output.add(element);
+        sub(nums,i+1,output,ans);
+        
+        //exclude
+        output.remove(output.size()-1);
+        sub(nums,i+1,output,ans);
+    }
+}
