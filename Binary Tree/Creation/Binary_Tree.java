3acb7c602491bb74c77cd96ef8220ef2c3f972de
soumyabag <67749624+soumyabag@users.noreply.github.com>
2022-11-12 17:54:40 +0530

    Add files via upload

---
+++ Binary Tree/Creation/Binary_Tree.java
@@ -0,0 +1,11 @@
+package Creation_Print_Binary_Trees;
+
+public class Binary_Tree<T> {
+       public T data;
+       public Binary_Tree<T> left;
+       public Binary_Tree<T> right;
+       
+       public Binary_Tree(T data){
+    	   this.data = data;
+       }
+}
